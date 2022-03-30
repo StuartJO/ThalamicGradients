040a6b2194ce56b496391fa8a0cb8370c99fa061
StuartJO <sjold1@student.monash.edu>
2022-03-31 10:14:20 +1100

    Updated heritability output

--- MakeData4Heritability.m
+++ MakeData4Heritability.m
@@ -1,24 +1,26 @@
-
-Output_MZ = nan(size(MZ_ID,1),4,2);
+load('twinCovariatesDWI.mat')
+Output_MZ = nan(size(MZ_ID,1),4,3);
 for i = 1:size(MZ_ID,1)
     for j = 1:4
         ID = MZ_ID(i,j);
         if ~isnan(ID)
            ID_IND = find(TWINSUBs==ID)+1;
            Output_MZ(i,j,1) = C_align(ID_IND,1); 
-           Output_MZ(i,j,2) = C_align(ID_IND,1); 
+           Output_MZ(i,j,2) = abs(C_orig(1,ID_IND-1)); 
+           Output_MZ(i,j,3) = abs(C_orig(2,ID_IND-1));
         end
     end
 end
 
-Output_DZ = nan(size(DZ_ID,1),4,2);
+Output_DZ = nan(size(DZ_ID,1),4,3);
 for i = 1:size(DZ_ID,1)
     for j = 1:4
         ID = DZ_ID(i,j);
         if ~isnan(ID)
            ID_IND = find(TWINSUBs==ID)+1;
            Output_DZ(i,j,1) = C_align(ID_IND,1); 
-           Output_DZ(i,j,2) = C_align(ID_IND,1); 
+           Output_DZ(i,j,2) = abs(C_orig(1,ID_IND-1)); 
+           Output_DZ(i,j,3) = abs(C_orig(2,ID_IND-1));
         end
     end
 end
